299526a581b0403a63be9fd32d9ec953c021c67f
CJ-ONYERO99 <chimeziejoshuaonyero999@gmail.com>
2025-02-06 15:19:19 -0500

    corrections made

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,6 @@
 import authConfig from "@/auth.config";
 import NextAuth from "next-auth";
 
+export const runtime = 'nodejs'; 
+
 export const { handlers, signIn, signOut, auth } = NextAuth(authConfig);
