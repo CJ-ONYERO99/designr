ee9e02f05cd16238e517d16e04f776b59e47f6b9
CJ-ONYERO99 <chimeziejoshuaonyero999@gmail.com>
2025-02-07 02:00:24 -0500

    env.local updated

--- src/auth.ts
+++ src/auth.ts
@@ -2,5 +2,6 @@ import authConfig from "@/auth.config";
 import NextAuth from "next-auth";
 
 export const runtime = 'nodejs'; 
-
-export const { handlers, signIn, signOut, auth } = NextAuth(authConfig);
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    ...authConfig
+});
